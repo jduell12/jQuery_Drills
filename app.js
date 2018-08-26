b7c64699bdec334268a5154504c1b402bc263368
Jessica Duell <duellrdh@gmail.com>
2018-08-26 14:10:49 -0400

    button enabled only when text is entered into field

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+//Checks to see that the HTML has loaded before javascript is run
 $(document).ready(function (){
+    //Button is diabled unless the user types input into the field 
+    $("#text").keyup(function (){
+        if ($(this).val() == ''){
+            $(".enableOnInput").prop('disabled', true);
+        } else {
+            $(".enableOnInput").prop('disabled', false);
+        }
+    })
+
+    //When button is clicked an alert tells the user the button has been clicked. If text is entered in the text box when the button is clicked then an alert with the inputted text is shown to the user
     $("#btnSubmit").on("click", function(){
         alert("The button has been clicked");
-    });
+        let txt = $("#text").val();
+        alert(txt);
+    })
 });
+
