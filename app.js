9982bc610797231615753e8d4d068a1f233025dd
Jessica Duell <duellrdh@gmail.com>
2018-08-26 17:50:57 -0400

    completed

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 //Checks to see that the HTML has loaded before javascript is run
-$(document).ready(function (){
+$(document).ready(function () {
+
+
     //Creating a div element and appending it to the body
-   $("body").append("<div></div>");
+    $("body").append("<div></div>");
+
+    //Create an unordered list and appending it to the body
+    $("body").append("<ul></ul>");
 
     //Button is diabled unless the user types input into the field 
-    $("#text").keyup(function (){
-        if ($(this).val() == ''){
+    $("#text").keyup(function () {
+        if ($(this).val() == '') {
             $(".enableOnInput").prop('disabled', true);
         } else {
             $(".enableOnInput").prop('disabled', false);
         }
-    })
+    });
 
     //When button is clicked an alert tells the user the button has been clicked. If text is entered in the text box when the button is clicked then an alert with the inputted text is shown to the user
-    $("#btnSubmit").on("click", function(){
+    $("#btnSubmit").click(function (event) {
+        event.preventDefault();
         alert("The button has been clicked");
         let txt = $("#text").val();
         alert(txt);
-        $("div").append("<h2>" + $("#text").val() + "</h2>");
-    })
+        $("ul").append("<li>" + txt + "</li>");
+        //When user clicks on li element it gets assigned a random color
+        $("li").click(function () {
+            $(this).css({ "color": rColor() });
+        });
+        $("li").dblclick(function () {
+            $(this).remove();
+        });
+    });
 
-   
+    //function to pick a random color
+    function rColor() {
+        let colorR = Math.floor((Math.random() * 256));
+        let colorG = Math.floor((Math.random() * 256));
+        let colorB = Math.floor((Math.random() * 256));
+        let color = "rgb(" + colorR + "," + colorG + "," + colorB + ")";
+        return color;
+    }
 
 });
 
